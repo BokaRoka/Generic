bb108912bb56f5cca4fa888d1f8d9785bd0fe59b
BokaRoka <mithun175@gmail.com>
2026-04-12 22:01:41 +0600

    1

--- Windows Update DISABLE.bat
+++ Windows Update DISABLE.bat
@@ -1,29 +1,42 @@
-reg.exe add "HKLM\Software\Microsoft\Windows\CurrentVersion\DeliveryOptimization\Config" /v DODownloadMode /t REG_DWORD /d 0 /f
+@echo off
+setlocal
+setlocal Enabledelayedexpansionreg.exe add "HKLM\Software\Microsoft\Windows\CurrentVersion\DeliveryOptimization\Config" /v DODownloadMode /t REG_DWORD /d 0 /f
 reg.exe add "HKLM\Software\Microsoft\Windows\CurrentVersion\DriverSearching" /v "DontSearchWindowsUpdate" /t REG_DWORD /d "1" /f
 reg.exe add "HKLM\Software\Microsoft\Windows\CurrentVersion\DriverSearching" /v "SearchOrderConfig" /t REG_DWORD /d "0" /f
+reg.exe add "HKLM\Software\Microsoft\Windows\CurrentVersion\DriverSearching" /v DontSearchWindowsUpdate /t REG_DWORD /d 1 /f
+reg.exe add "HKLM\Software\Microsoft\Windows\CurrentVersion\DriverSearching" /v SearchOrderConfig /t REG_DWORD /d 0 /f
 reg.exe add "HKLM\Software\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update" /v "IncludeRecommendedUpdates" /t REG_DWORD /d "0" /f
+reg.exe add "HKLM\Software\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update" /v IncludeRecommendedUpdates /t REG_DWORD /d 0 /f
 reg.exe add "HKLM\Software\Microsoft\WindowsUpdate\UX\Settings" /v "UxOption" /t REG_DWORD /d "1" /f
 reg.exe add "HKLM\Software\Microsoft\WindowsUpdate\UX\Settings" /v ExcludeWUDriversInQualityUpdate /t REG_DWORD /d 1 /f
+reg.exe add "HKLM\Software\Microsoft\WindowsUpdate\UX\Settings" /v UxOption /t REG_DWORD /d 1 /f
 reg.exe add "HKLM\Software\Policies\Microsoft\Windows\WindowsUpdate" /v "DisableWindowsUpdateAccess" /t REG_DWORD /d "1" /f
+reg.exe add "HKLM\Software\Policies\Microsoft\Windows\WindowsUpdate" /v DisableWindowsUpdateAccess /t REG_DWORD /d 1 /f
 reg.exe add "HKLM\Software\Policies\Microsoft\Windows\WindowsUpdate" /v ExcludeWUDriversInQualityUpdate /t REG_DWORD /d 1 /f
-reg.exe add "HKLM\System\CurrentControlSet\Services\BITS" /v "start" /t REG_DWORD /d "4" /f
-reg.exe add "HKLM\System\CurrentControlSet\Services\DoSvc" /v "start" /t REG_DWORD /d "4" /f
-reg.exe add "HKLM\System\CurrentControlSet\Services\UsoSvc" /v "start" /t REG_DWORD /d "4" /f
-reg.exe add "HKLM\System\CurrentControlSet\Services\WaaSMedicSvc" /v "start" /t REG_DWORD /d "4" /f
-reg.exe add "HKLM\System\CurrentControlSet\Services\wuauserv" /v "start" /t REG_DWORD /d "4" /f
+reg.exe add "HKLM\System\CurrentControlSet\Services\BITS" /v Start /t REG_DWORD /d 4 /f
+reg.exe add "HKLM\System\CurrentControlSet\Services\DoSvc" /v Start /t REG_DWORD /d 4 /f
+reg.exe add "HKLM\System\CurrentControlSet\Services\UsoSvc" /v Start /t REG_DWORD /d 4 /f
+reg.exe add "HKLM\System\CurrentControlSet\Services\WaaSMedicSvc" /v Start /t REG_DWORD /d 4 /f
+reg.exe add "HKLM\System\CurrentControlSet\Services\wuauserv" /v Start /t REG_DWORD /d 4 /f
 schtasks.exe /change /tn "\Microsoft\Windows\WindowsUpdate\AUFirmwareInstall" /Disable
 schtasks.exe /change /tn "\Microsoft\Windows\WindowsUpdate\AUScheduledInstall" /Disable
 schtasks.exe /change /tn "\Microsoft\Windows\WindowsUpdate\AUSessionConnect" /Disable
 schtasks.exe /change /tn "\Microsoft\Windows\WindowsUpdate\Automatic App Update" /Disable
 schtasks.exe /change /tn "\Microsoft\Windows\WindowsUpdate\Scheduled Start" /Disable
+schtasks.exe /change /tn "\Microsoft\Windows\WindowsUpdate\sih" /Disable
+schtasks.exe /change /tn "\Microsoft\Windows\WindowsUpdate\sihboot" /Disable
+schtasks.exe /change /tn "\Microsoft\Windows\WindowsUpdate\sihpostreboot" /Disable
 schtasks.exe /change /tn "Microsoft\Windows\WindowsUpdate\Automatic App Update" /Disable
-schtasks.exe /change /tn "Microsoft\Windows\WindowsUpdate\Scheduled start" /Disable
+schtasks.exe /change /tn "Microsoft\Windows\WindowsUpdate\Scheduled Start" /Disable
 schtasks.exe /change /tn "Microsoft\Windows\WindowsUpdate\sih" /Disable
 schtasks.exe /change /tn "Microsoft\Windows\WindowsUpdate\sihboot" /Disable
 schtasks.exe /change /tn "Microsoft\Windows\WindowsUpdate\sihpostreboot" /Disable
+@echo off & setlocal & call powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "Get-ScheduledTask | Where-Object { $_.TaskName -like '*InstallService*' -or $_.TaskName -like '*UpdateOrchestrator*' -or $_.TaskName -like '*UpdateAssistant*' -or $_.TaskName -like '*WaaSMedic*' -or $_.TaskName -like '*WindowsUpdate*' } | ForEach-Object { try { Unregister-ScheduledTask -TaskName $_.TaskName -Confirm:$false -ErrorAction Stop; Write-Host 'Removed:' $_.TaskName } catch { Write-Host 'Failed:' $_.TaskName } }" & endlocal
+@echo off & setlocal & call powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "Get-ScheduledTask | Where-Object { $_.TaskName -like '*Google*' -or $_.TaskName -like '*wps*' -or $_.TaskName -like '*Mozilla*' -or $_.TaskName -like '*bonjour*' -or $_.TaskName -like '*Avast*' -or $_.TaskName -like '*Edge*' -or $_.TaskName -like '*Opera*' -or $_.TaskName -like '*EdgeUpdate*' -or $_.TaskName -like '*Brave*' -or $_.TaskName -like '*xbox*' -or $_.TaskName -like '*onedrive*' -or $_.TaskName -like '*bluestacks*' -or $_.TaskName -like '*AMD*' } | ForEach-Object { try { Unregister-ScheduledTask -TaskName $_.TaskName -Confirm:$false -ErrorAction Stop; Write-Host 'Removed:' $_.TaskName } catch { Write-Host 'Failed:' $_.TaskName } }" & endlocal
 
 pause
 
+setlocal
 reg.exe add "HKLM\Software\Microsoft\Windows\CurrentVersion\DeliveryOptimization\Config" /v DODownloadMode /t REG_DWORD /d "1" /f
 reg.exe add "HKLM\Software\Microsoft\Windows\CurrentVersion\DriverSearching" /v "DontSearchWindowsUpdate" /t REG_DWORD /d "0" /f
 reg.exe add "HKLM\Software\Microsoft\Windows\CurrentVersion\DriverSearching" /v "SearchOrderConfig" /t REG_DWORD /d "1" /f
@@ -32,11 +45,11 @@ reg.exe add "HKLM\Software\Microsoft\WindowsUpdate\UX\Settings" /v "UxOption" /t
 reg.exe add "HKLM\Software\Microsoft\WindowsUpdate\UX\Settings" /v ExcludeWUDriversInQualityUpdate /t REG_DWORD /d "0" /f
 reg.exe add "HKLM\Software\Policies\Microsoft\Windows\WindowsUpdate" /v "DisableWindowsUpdateAccess" /t REG_DWORD /d "0" /f
 reg.exe add "HKLM\Software\Policies\Microsoft\Windows\WindowsUpdate" /v ExcludeWUDriversInQualityUpdate /t REG_DWORD /d "0" /f
-reg.exe add "HKLM\System\CurrentControlSet\Services\BITS" /v "start" /t REG_DWORD /d "2" /f
-reg.exe add "HKLM\System\CurrentControlSet\Services\DoSvc" /v "start" /t REG_DWORD /d "2" /f
-reg.exe add "HKLM\System\CurrentControlSet\Services\UsoSvc" /v "start" /t REG_DWORD /d "2" /f
-reg.exe add "HKLM\System\CurrentControlSet\Services\WaaSMedicSvc" /v "start" /t REG_DWORD /d "2" /f
-reg.exe add "HKLM\System\CurrentControlSet\Services\wuauserv" /v "start" /t REG_DWORD /d "2" /f
+reg.exe add "HKLM\System\CurrentControlSet\Services\BITS" /v Start /t REG_DWORD /d 2 /f
+reg.exe add "HKLM\System\CurrentControlSet\Services\DoSvc" /v Start /t REG_DWORD /d 2 /f
+reg.exe add "HKLM\System\CurrentControlSet\Services\UsoSvc" /v Start /t REG_DWORD /d 2 /f
+reg.exe add "HKLM\System\CurrentControlSet\Services\WaaSMedicSvc" /v Start /t REG_DWORD /d 2 /f
+reg.exe add "HKLM\System\CurrentControlSet\Services\wuauserv" /v Start /t REG_DWORD /d 2 /f
 schtasks.exe /change /tn "\Microsoft\Windows\InstallService\ScanForUpdates" /Enable
 schtasks.exe /change /tn "\Microsoft\Windows\InstallService\ScanForUpdatesAsUser" /Enable
 schtasks.exe /change /tn "\Microsoft\Windows\InstallService\SmartRetry" /Enable
@@ -54,7 +67,19 @@ schtasks.exe /change /tn "\Microsoft\Windows\WindowsUpdate\Scheduled Start" /Ena
 schtasks.exe /change /tn "\Microsoft\Windows\WindowsUpdate\sih" /Enable
 schtasks.exe /change /tn "\Microsoft\Windows\WindowsUpdate\sihboot" /Enable
 schtasks.exe /change /tn "Microsoft\Windows\WindowsUpdate\Automatic App Update" /Enable
-schtasks.exe /change /tn "Microsoft\Windows\WindowsUpdate\Scheduled start" /Enable
+schtasks.exe /change /tn "Microsoft\Windows\WindowsUpdate\Scheduled Start" /Enable
 schtasks.exe /change /tn "Microsoft\Windows\WindowsUpdate\sih" /Enable
 schtasks.exe /change /tn "Microsoft\Windows\WindowsUpdate\sihboot" /Enable
 schtasks.exe /change /tn "Microsoft\Windows\WindowsUpdate\sihpostreboot" /Enable
+@echo off && powershell.exe -ExecutionPolicy Bypass -Command "Get-ScheduledTask | Where-Object { $_.TaskName -like '*InstallService*' -or $_.TaskName -like '*UpdateOrchestrator*' -or $_.TaskName -like '*UpdateAssistant*' -or $_.TaskName -like '*WaaSMedic*' -or $_.TaskName -like '*WindowsUpdate*' } | Enable-ScheduledTask -ErrorAction SilentlyContinue"
+endlocal
+
+
+
+
+
+
+
+
+
+
